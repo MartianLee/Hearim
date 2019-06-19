2343b514848bd5dbcf3d66d9d5541ecac7a72af7
MartianLee <martionlee@gmail.com>
2019-06-20 02:52:19 +0900

    Update seed, style and Add Favicon

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create( email: 'martionlee@gmail.com', password: '123456' )
-user = User.find(1)
+user = User.first
 user.add_role :admin
 
 user = User.create( email: 'leejoonsoo95@naver.com', password: '123456' )
-user = User.find(2)
+user = User.second
 user.add_role :admin
+
+today_sentence = TodaySentence.create( date: '2019/06/19', body: '오늘 하루 정말 수고했어!' )
+today_sentence = TodaySentence.create( date: '2019/06/20', body: '아침에 잘 일어났구나' )
