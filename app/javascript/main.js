264141a745e57ffc6aae9f9497d6f629bcc8acdf
MartianLee <martionlee@gmail.com>
2019-06-20 19:08:25 +0900

    Add copy message

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,4 +1,4 @@
-var $ = require('jquery')
+global.$ = require('jquery')
 
 window.autosize = function() {
     var el = $("#form-text")[0];
@@ -17,3 +17,28 @@ $( document ).ready(function() {
 });
 
 $("#form-text").bind("keydown", autosize);
+
+window.copyLink = function(id) {
+    // window.clipboardData.setData("Text", url);
+    // var copyText = document.getElementById(url);
+
+    // /* Select the text field */
+    // copyText.select();
+
+    // /* Copy the text inside the text field */
+    // document.execCommand("Copy");
+
+    // console.log("Copied the text: " + copyText.value);
+    var val = document.getElementById('js-url-' + id).value;
+    var t = document.createElement("textarea");
+    document.body.appendChild(t);
+    t.value = val;
+    t.select();
+    document.execCommand('copy');
+    document.body.removeChild(t);
+
+    var message = '#js-copy-message-' + id;
+    $(message).css("opacity",1);
+    setTimeout(function(){ $(message).css("opacity",0); }, 1000);
+
+}
