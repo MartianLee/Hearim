2e69c8b86da90c93859fe1879e9c7e93d07ee3a9
MartianLee <martionlee@gmail.com>
2019-06-21 02:18:14 +0900

    Change like and save method to remote

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,24 +7,16 @@ class LikesController < ApplicationController
           return
         end
         liked = Like.find_by(user_id: current_user.id, letter_id: params[:letter])
+        @letter = Letter.find_by(id: params[:letter])
         if liked.present?
           liked.destroy
-          redirect_to(letters_path)
           return
         end
 
         like = Like.new
         like.user_id = current_user.id
         like.letter_id = params[:letter]
-        respond_to do |format|
-          if like.save
-            format.html { redirect_to letters_path, notice: 'Like was successfully created.' }
-            format.json { render :show, status: :created, location: letters_path }
-          else
-            # format.html { redirect_to letters_path, notice: 'Like failed' }
-            format.json { render json: 'errors', status: :unprocessable_entity }
-          end
-        end
+        like.save
     end
     def destroy
         @like.destroy
--- app/controllers/saved_letters_controller.rb
+++ app/controllers/saved_letters_controller.rb
@@ -8,24 +8,16 @@ class SavedLettersController < ApplicationController
           return
         end
         saved = SavedLetter.find_by(user_id: current_user.id, letter_id: params[:letter])
+        @letter = Letter.find_by(id: params[:letter])
         if saved.present?
           saved.destroy
-          redirect_to(letters_path)
           return
         end
 
         save = SavedLetter.new
         save.user_id = current_user.id
         save.letter_id = params[:letter]
-        respond_to do |format|
-          if save.save
-            format.html { redirect_to letters_path, notice: 'Save was successfully created.' }
-            format.json { render :show, status: :created, location: letters_path }
-          else
-            # format.html { redirect_to letters_path, notice: 'Like failed' }
-            format.json { render json: 'errors', status: :unprocessable_entity }
-          end
-        end
+        save.save
     end
     def destroy
         @save.destroy
