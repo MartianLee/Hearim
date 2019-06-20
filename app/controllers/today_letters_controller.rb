a006db6cc9486a9943a47292a611ce049b9b1677
MartianLee <martionlee@gmail.com>
2019-06-21 02:38:48 +0900

    Fix error in today_letter controller and page

--- app/controllers/today_letters_controller.rb
+++ app/controllers/today_letters_controller.rb
@@ -2,14 +2,23 @@ class TodayLettersController < ApplicationController
     def index
         @today_letters = Letter.where(id: TodayLetter.all.pluck(:letter_id)).order(created_at: :desc)
     end
+    def best_liked
+        @today_letter.letter_id = Letter.where(created_at: selected_date.beginning_of_day..selected_date.end_of_day).order(:like_count).last
+    end
     def new
         @today_letter = TodayLetter.new
         selected_date = DateTime.now.to_date
-        @today_letter.letter_id = Letter.where(:created_at => selected_date.beginning_of_day..selected_date.end_of_day).order(:like_count).last.id
+        @today_letter.day = DateTime.now.to_date
+        best_letter = Letter.where(created_at: selected_date.beginning_of_day..selected_date.end_of_day).order(:like_count).last
+        if best_letter.blank? 
+          best_letter = Letter.where("created_at <= ?", selected_date.beginning_of_day).order(:like_count).last
+        end
+        @today_letter.letter_id = best_letter.id
     end
     def create
         @today_letter = TodayLetter.new(today_letter_params)
         @today_letter.save
+        redirect_to admini_path
     end
     def update
         @today_letter.update(today_letter_params)
