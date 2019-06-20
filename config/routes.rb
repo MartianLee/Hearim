72e097d468578dfc797141eb8cc004c71aacf2fe
MartianLee <martionlee@gmail.com>
2019-06-20 17:59:27 +0900

    Add info rule and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :letters
-  resources :likes
   resources :saved_letters
   resources :today_letters
   resources :today_sentences
   get '/admini', to: 'admin#index'
   get '/my_letters', to: 'letters#my_letters'
   get '/about', to: 'about#index'
+  get '/info', to: 'about#info'
   get '/menu', to: 'timeline#index'
   root 'about#index'
+  post '/likes', to: 'likes#create'
 
    # error pages
    %w( 404 422 500 503 ).each do |code|
